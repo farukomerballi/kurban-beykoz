eb01122a5970995d7f0221b542db768510714040
farukomerballi <farukomerballi@gmail.com>
2026-04-13 02:41:01 +0300

    fix: conditional basePath for dev/prod environments

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: '/beykozkurban',
+  basePath: process.env.NODE_ENV === 'production' ? '/beykozkurban' : '',
   images: {
     unoptimized: true,
   },
